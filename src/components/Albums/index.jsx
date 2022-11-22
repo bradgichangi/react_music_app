e8e8fad7932b045de1cab845c64d2c24b623da81
Bradley Gichangi <bradgichangi@gmail.com>
2022-11-22 11:36:35 +0000

    Updated JS

--- src/components/Albums/index.jsx
+++ src/components/Albums/index.jsx
@@ -5,6 +5,8 @@ const Albums = ({data}) => {
     return (
         <div className="artist">
             <h2>Album Name</h2>
+            <p>Genre</p>
+            <p>Date</p>
             <p>Track list here</p>
         </div>
     )
--- src/components/Artist/index.jsx
+++ src/components/Artist/index.jsx
@@ -6,6 +6,8 @@ const Artist = ({data}) => {
     return (
         <div className="artist">
             <h2>Name</h2>
+            <p>Bio</p>
+            <p>Genre</p>
             <p>Album list here</p>
         </div>
     )
