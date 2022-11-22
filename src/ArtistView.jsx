88b1caa8c488256c59032a6d20e832cc9ee93dad
Bradley Gichangi <bradgichangi@gmail.com>
2022-11-22 17:04:56 +0000

    Updated frontend

--- src/ArtistView.jsx
+++ src/ArtistView.jsx
@@ -9,8 +9,19 @@ const ArtistView = () => {
 
     return (
         <>
+        <header>
+            <h1 id='title'>SpeakerBox</h1>
+        </header>
         <h1>Drake</h1>
-        <img src={data.img} alt={data.name} />
+        <img src='https://www.theaudiodb.com/images/media/artist/thumb/vpxqtu1484769349.jpg' alt='' />
+        <p>Genre</p>
+        <div className='genres'>
+            <div className="genre">Hip-Hop</div>
+            <div className="genre">Rap</div>
+            <div className="genre">R&B</div>
+        </div>
+        <p>Record Label</p>
+        <img src="https://www.theaudiodb.com/images/media/label/logo/vwptyr1532902436.png" alt="" />
         </>
     )
 }
